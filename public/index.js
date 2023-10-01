accd2a9655834b5f66e7e7fe280e331ac63d92ca
Abdulhafiz <abdulhafizaderemi@gmail.com>
2023-10-01 22:14:46 +0100

    initial commit

--- public/index.js
+++ public/index.js
@@ -18,28 +18,28 @@ async function uploadFiles() {
     });
     const data = await response.json();
     console.log(data);
+  
+    document.querySelector("input").value = "";
+  
+    if (response.status !== 200) {
+      const msg = data.msg;
+      responseDOM.innerHTML = `<p class="text-light">${msg}</p>`;
+    } else {
+      responseDOM.innerHTML = `<p class="text-light">Uploaded successfully. View your uploads:</p>`;
+      Object.values(data)[2]
+        .split(",")
+        .forEach((imagePath) => {
+          // console.log(imagePath);
+          const pElem = document.createElement("p");
+          const elem = document.createElement("a");
+          elem.href = `${imagePath}`;
+          elem.innerText = `${imagePath}`;
+          pElem.appendChild(elem);
+          responseDOM.appendChild(pElem);
+        });
+    }
   } catch (error) {
     console.log(error);
-  }
-
-  document.querySelector("input").value = "";
-
-  if (response.status !== 200) {
-    const msg = data.msg;
-    responseDOM.innerHTML = `<p class="text-light">${msg}</p>`;
-  } else {
-    responseDOM.innerHTML = `<p class="text-light">Uploaded successfully. View your uploads:</p>`;
-    Object.values(data)[2]
-      .split(",")
-      .forEach((imagePath) => {
-        // console.log(imagePath);
-        const pElem = document.createElement("p");
-        const elem = document.createElement("a");
-        elem.href = `${imagePath}`;
-        elem.innerText = `${imagePath}`;
-        pElem.appendChild(elem);
-        responseDOM.appendChild(pElem);
-      });
   }
 }
 
